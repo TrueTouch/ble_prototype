9d36cbce7698565de9786d46f4516e9f1ca32758
Cameron Kluza <cmkluza@gmail.com>
2020-11-17 02:56:20 -0500

    Minor changes to original hard-coded app example Removes old comment and changes some pins

--- desktop/main.py
+++ desktop/main.py
@@ -1,16 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Notifications
--------------
-Example showing how to add notifications to a characteristic and handle the responses.
-Updated on 2019-07-03 by hbldh <henrik.blidh@gmail.com>
-"""
 
 import asyncio
-
 import time
-
-from bleak import _logger as logger
 
 from ble_uart_pin_ctrl import BleUartPinCtrl
 
@@ -26,19 +17,19 @@ async def run():
     time.sleep(5)
 
     print("Sending GPIO configure")
-    await pin_ctrl.configure_gpio(port=0, pins=[3, 12], is_output=True)
+    await pin_ctrl.configure_gpio(port=0, pins=[12], is_output=True)
     print("GPIO configure sent")
 
     time.sleep(5)
 
     print("Sending GPIO write")
-    await pin_ctrl.write_gpio(port=0, pins=[3], output_high=True)
+    await pin_ctrl.write_gpio(port=0, pins=[12], output_high=True)
     print("GPIO write sent")
 
-    time.sleep(5)
+    time.sleep(0.01)
 
     print("Sending PWM set")
-    await pin_ctrl.set_pwm(port=0, pins=[12], intensity=125)
+    await pin_ctrl.write_gpio(port=0, pins=[12], output_high=False)
     print("PWM set sent")
 
     while True:
